14c7e6e2ce33ab4f93486394621b1e1b871cc615
Jeon <dbs_dmswl92@naver.com>
2022-05-11 00:54:09 +0900

    EmulatorMove

---
+++ Assets/JHWPractice/cshEmulatorMoving.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshEmulatorMoving : MonoBehaviour
+{
+    
+    float mSpeed = 2.0f;
+
+    private Quaternion mCharacterTargetRot;
+    private Quaternion mCameraTargetRot;
+    public Transform mPlayerCamera;
+
+    void Start()
+    {
+        mCharacterTargetRot = transform.localRotation;
+        mCameraTargetRot = mPlayerCamera.transform.localRotation;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+ 
+    void Update()
+    {
+        float xRot = Input.GetAxis("Mouse Y");
+        float yRot = Input.GetAxis("Mouse X");
+
+        mCharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+        transform.localRotation = mCharacterTargetRot;
+
+        mCameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+        mPlayerCamera.transform.localRotation = mCameraTargetRot;
+
+        float vmv = Input.GetAxis("Vertical");
+        float hmv = Input.GetAxis("Horizontal");
+
+        Vector2 mInput = new Vector2(hmv, vmv);
+        Vector3 desireMove = transform.forward * 1 * mInput.y +
+            transform.right * 1 * mInput.x;
+        transform.position += desireMove * mSpeed * Time.deltaTime;
+       
+
+
+    }
+}
---
+++ Assets/JHWPractice/cshPlayerMoving.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshPlayerMoving : MonoBehaviour
+{
+    //PlayerEmulator var
+    private Vector3 m_velocity;
+    public float m_moveSpeed = 2.0f;
+
+
+    //OculusPractice var
+    public int speedForward = 6;
+    public int speedSide = 6;
+    private float dirX = 0;
+    private float dirZ = 0;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        PlayerMove();
+        //OculusPlayerMoving();
+    }
+
+    
+    //PlayerMovingEmulator
+    private void PlayerMove()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        m_velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        m_velocity = m_velocity.normalized;
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            m_velocity *= 2.0f;
+        }
+
+        controller.Move(m_velocity * m_moveSpeed * Time.deltaTime);
+       
+    }
+    
+
+    //Oculus PlayerMoveing Emulaotr
+
+    private void OculusPlayerMoving()
+    {
+        dirX = 0;
+        dirZ = 0;
+        if(OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))
+        {
+            Vector2 coord = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+            var absX = Mathf.Abs(coord.x);
+            var absY = Mathf.Abs(coord.y);
+
+            if(absX > absY)
+            {
+                //Right
+                if(coord.x > 0)
+                {
+                    dirX = 1;
+                }
+                else
+                {
+                    dirX = -1;
+                }
+            }
+            else
+            {
+                if(coord.y > 0)
+                {
+                    dirZ = 1;
+                }
+                else
+                {
+                    dirZ = -1;
+                }
+            }
+            Vector3 moveDir = new Vector3(dirX * speedSide, 0, dirZ * speedForward);
+            transform.Translate(moveDir * Time.smoothDeltaTime);
+        }
+    }
+}
