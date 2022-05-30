f688158b329a283e76c090892deb404f22b10efd
SeoungHan <73932680+SeoungHan@users.noreply.github.com>
2022-05-31 01:03:42 +0900

    책 캔버스
    1. 색깔 별로 책 캔버스 제작
    2. 디테일한 트리거 스크립트는 부탁드림

---
+++ Assets/Scripts/BookManager/BookClose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookClose : MonoBehaviour
+{
+    public GameObject m_CancelRedCanvas;
+
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        m_CancelRedCanvas.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/BookManager/RedBook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBook : MonoBehaviour
+{
+    public GameObject m_RedCanvas;
+
+    public void Open()
+    {
+        m_RedCanvas.SetActive(true);
+    }
+
+
+    public void Close()
+    {        
+        gameObject.SetActive(false);
+    }
+}
