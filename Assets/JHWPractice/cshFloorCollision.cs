7071d82f37a8481318b19b5f9a3c9d69727e2622
Jeon <dbs_dmswl92@naver.com>
2022-06-01 22:16:50 +0900

    Floor and Chess Collision
    바닥과 체스가 충돌했을 때, 체스를 잃어버리지 않도록 체스를 체스판 근처로 다시 위치시킨다.

---
+++ Assets/JHWPractice/cshFloorCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshFloorCollision : MonoBehaviour
+{
+    public GameObject m_QueenArea;
+    public GameObject m_KingArea;
+    public GameObject m_KnightArea;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Chess")
+        {
+            if(collision.gameObject.name == "Queen")
+            {
+                collision.transform.position = m_QueenArea.transform.position;
+            }
+            else if(collision.gameObject.name == "King")
+            {
+                collision.transform.position = m_KingArea.transform.position;
+            }
+            else if(collision.gameObject.name == "Knight")
+            {
+                collision.transform.position = m_KnightArea.transform.position;
+            }
+        }
+    }
+
+}
