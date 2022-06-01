5ff2e51e5c32fbb61a35b26ec39b256611f6d6da
KLMIL <kanoline@naver.com>
2022-06-01 20:20:22 +0900

    화로 기믹 기능 생성
    답 맞으면 Debug.Log만 띄우는 상태.
    불 꺼지는 기능 구현해야함

---
+++ Assets/4.Scripts/NormalButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityEventNormalButton : UnityEvent<char>
+{
+
+}
+
+public class NormalButton : MonoBehaviour
+{
+    public UnityEventNormalButton unityEvent;
+
+    [SerializeField] GameObject button;
+    [SerializeField] char btnColor; // 버튼 색
+
+    public void OnClick()
+    {
+        unityEvent.Invoke(btnColor);
+    }
+}
---
+++ Assets/4.Scripts/NormalButtonController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalButtonController : MonoBehaviour
+{
+    [SerializeField] bool[] correctAnswer = { true, true, false, false, true, false, true };
+    [SerializeField] bool[] userAnswer = { false, false, false, false, false, false, false };
+
+    [SerializeField] GameObject[] display;
+
+    private int userAnsIndex = 0;
+
+    public void IsCorrectAnswer()
+    {
+        int correct = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            if (userAnswer[i] == true && correctAnswer[i] == userAnswer[i])
+            {
+                correct++;
+            }
+        }
+
+        if (correct == 4)
+        {
+            OpenLock();
+        }
+        else
+        {
+            Debug.Log("No Answer.." + correct);
+        }
+    }
+
+    public void OpenLock()
+    {
+        Debug.Log("Blazier Open");
+    }
+
+
+
+
+    // btnColor : 버튼 색 (B O Y N R G P, S(RESET))
+    public void pressButton(char btnColor)
+    {
+        if (btnColor == 'S') // SUBMIT
+        {
+            IsCorrectAnswer();
+        }
+
+        if (btnColor == 'T') // RESET
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                display[i].GetComponent<Renderer>().material.color = new Color(255, 255, 255, 0.5f);
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                userAnswer[i] = false;
+            }
+            userAnsIndex = 0;
+        }
+        
+        if (userAnsIndex < 4)
+        {
+            if (btnColor == 'B' && userAnswer[0] != true)
+            {
+                userAnswer[0] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(0, 0.33f, 1f, 0.5f);
+            }
+            else if (btnColor == 'O' && userAnswer[1] != true)
+            {
+                userAnswer[1] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(1f, 0.32f, 0, 0.5f);
+            }
+            else if (btnColor == 'Y' && userAnswer[2] != true)
+            {
+                userAnswer[2] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(1f, 0.78f, 0, 0.5f);
+            }
+            else if (btnColor == 'N' && userAnswer[3] != true)
+            {
+                userAnswer[3] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(0.24f, 0.02f, 0.6f, 0.5f);
+            }
+            else if (btnColor == 'R' && userAnswer[4] != true)
+            {
+                userAnswer[4] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(1f, 0, 0, 0.5f);
+            }
+            else if (btnColor == 'G' && userAnswer[5] != true)
+            {
+                userAnswer[5] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(0.7f, 1f, 0, 0.5f);
+            }
+            else if (btnColor == 'P' && userAnswer[6] != true)
+            {
+                userAnswer[6] = true;
+                display[userAnsIndex++].GetComponent<Renderer>().material.color = new Color(0.58f, 0, 0.97f, 0.5f);
+            }
+        }
+    }
+}
