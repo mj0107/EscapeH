9ba005f3fc61045e69028d8ba6323587e2922097
KLMIL <kanoline@naver.com>
2022-06-02 04:41:16 +0900

    체스기믹 자리 검출
    버그나도 난 모름

---
+++ Assets/4.Scripts/ChessGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessGame : MonoBehaviour
+{
+    static public ChessGame Instance;
+
+    [SerializeField] GameObject[] chess = new GameObject[3];
+    public GameObject[] userAnswer = new GameObject[3];
+
+
+    public int count = 0;
+
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+
+    private void Update()
+    {
+        if (count == 3)
+        {
+            isCorrectAnswer();
+            count++;
+        }
+    }
+
+    private void isCorrectAnswer()
+    {
+        if (chess[0] == userAnswer[0] && chess[1] == userAnswer[1] && chess[2] == userAnswer[2])
+        {
+            Debug.Log("Correct!! godd!");
+        }
+
+    }
+
+    public void addCount()
+    {
+        count++;
+    }
+
+    public void minusCount()
+    {
+        count--;
+    }
+}
---
+++ Assets/4.Scripts/WhoTouchMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhoTouchMe : MonoBehaviour
+{
+    [SerializeField] int placeNum;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ChessGame.Instance.userAnswer[placeNum] = other.gameObject;
+    }
+}
