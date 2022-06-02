53b65e8083d0d60ad54d8c6ca74ce5db042193e2
Jeon <dbs_dmswl92@naver.com>
2022-06-02 16:15:41 +0900

    ARoomChessGimic_Script
    A방 체스 기믹 스크립트
    (B방과 동일한 스크립트 작성)

---
+++ Assets/JHWPractice/cshARoomChessGame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshARoomChessGame : MonoBehaviour
+{
+    static public cshARoomChessGame Instance;
+
+    [SerializeField] GameObject[] ARoomChess = new GameObject[4];
+
+    public GameObject[] ARoomChessAnswer = new GameObject[4];
+
+    public int ARoomChessCount = 0;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ARoomChessCount == 4)
+        {
+            CorrectAnswers();
+        }
+    }
+
+    private void CorrectAnswers()
+    {
+        if(ARoomChess[0] == ARoomChessAnswer[0] &&
+            ARoomChess[1] == ARoomChessAnswer[1] &&
+            ARoomChess[2] == ARoomChessAnswer[2] &&
+            ARoomChess[3] == ARoomChessAnswer[3] )
+        {
+            Debug.Log("Clear ARoom Chess Gimic");
+            /*
+             * A Room Player가 Chess Gimic을 수행했다는 정보를 
+             * GameManager에게 알려야 함.
+             */
+        }
+    }
+
+    public void CorrectAnswer()
+    {
+        ARoomChessCount++;
+    }
+
+    public void IncorrectAnswer()
+    {
+        ARoomChessCount--;
+    }
+}
---
+++ Assets/JHWPractice/cshCheckCollisionARoomChess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshCheckCollisionARoomChess : MonoBehaviour
+{
+    [SerializeField] int CorrectNum;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cshARoomChessGame.Instance.ARoomChessAnswer[CorrectNum] = other.gameObject;
+    }
+}
