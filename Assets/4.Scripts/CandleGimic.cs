1b7d24a9dfdfe15b0fcc69568a2ddc667be90e09
KLMIL <kanoline@naver.com>
2022-06-02 21:18:56 +0900

    캔들 기믹 완성

---
+++ Assets/4.Scripts/CandleGimic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityEventCandle : UnityEvent<char, int>
+{
+
+}
+public class CandleGimic : MonoBehaviour
+{
+    public UnityEventCandle unityEvent;
+
+    [SerializeField] GameObject button;
+
+    [SerializeField] char SL;
+    [SerializeField] int num;
+
+    public void OnClick()
+    {
+        unityEvent.Invoke(SL, num);
+    }
+}
---
+++ Assets/4.Scripts/CandleGimicController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleGimicController : MonoBehaviour
+{
+    [SerializeField] GameObject[] shortCandle = new GameObject[6];
+    [SerializeField] GameObject[] longCandle = new GameObject[4];
+
+    private bool[] sIsOn = new bool[6];
+    private bool[] lIsOn = new bool[4];
+
+    int shortCandleCount = 0;
+    int longCandleCount = 0;
+
+
+    // 종료함수
+    private void Update()
+    {
+        if (shortCandleCount == 1 && longCandleCount == 1)
+        {
+            OpenLock();
+            shortCandleCount = 100;
+            longCandleCount = 100;
+        }
+    }
+
+
+    // 종료행동함수
+    public void OpenLock()
+    {
+        Debug.Log("Candle Gimic clear!");
+    }
+
+
+
+    private void OnClickCandle(int i, char c)
+    {
+        if (c == 'S')
+        {
+            if (sIsOn[i])
+            {
+                shortCandle[i].GetComponentInChildren<ParticleSystem>().Stop();
+                sIsOn[i] = false;
+                shortCandleCount--;
+            }
+            else
+            {
+                shortCandle[i].GetComponentInChildren<ParticleSystem>().Play();
+                sIsOn[i] = true;
+                shortCandleCount++;
+            }
+        }
+        else
+        {
+            if (lIsOn[i])
+            {
+                longCandle[i].GetComponentInChildren<ParticleSystem>().Stop();
+                lIsOn[i] = false;
+                longCandleCount--;
+            }
+            else
+            {
+                longCandle[i].GetComponentInChildren<ParticleSystem>().Play();
+                lIsOn[i] = true;
+                longCandleCount++;
+            }
+        }
+    }
+
+
+    // btnColor : 버튼 색 (B O Y N R G P, S(RESET))
+    public void pressButton(char SL, int num)
+    {
+        OnClickCandle(num, SL);
+    }
+}
